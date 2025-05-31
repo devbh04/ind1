82710a985da33f30522d1e060bf09a5f56903b49
devbh04 <devbhangale.work@gmail.com>
2025-05-31 21:33:42 +0530

    major update

--- components/shared/cards/hackathoncard.tsx
+++ components/shared/cards/hackathoncard.tsx
@@ -136,7 +136,7 @@ const HackathonCard = ({ hackathon }: { hackathon: Hackathon }) => {
         </div>
 
         {/* CTA Button */}
-        <Link href={`/hackathons/${hackathon.id}`}>
+        <Link href={`/hackathon/hackathon-id`}>
           <div className="w-full mt-4 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 text-center flex items-center justify-center">
             View Details <ArrowRightIcon className="h-4 w-4 ml-2" />
           </div>
